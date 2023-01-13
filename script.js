03eb4266cb57499a454b48555017a091c4f5022a
Kadir Tastepe <70476992+kadirtastepe@users.noreply.github.com>
2023-01-14 00:05:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ toggle.addEventListener('click', function(){
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
-        body.style.transition = '2s';
+        body.style.transition = '1s';
     }else{
         body.style.background = 'black';
         body.style.color = 'white';
-        body.style.transition = '2s';
+        body.style.transition = '1s';
     }
 });
