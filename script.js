4097f119cbc24e6732e742687fb8c623b4195fb4
Kadir Tastepe <70476992+kadirtastepe@users.noreply.github.com>
2023-01-27 14:30:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ const body = document.querySelector('body');
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = 'white';
-        body.style.color = 'black';
-        body.style.transition = '1s';
-    }else{
-        body.style.background = 'black';
-        body.style.color = 'white';
-        body.style.transition = '1s';
-    }
     tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
@@ -27,6 +16,18 @@ toggle.addEventListener('click', function(){
     target.classList.add('active')
   })
 })
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '1s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '1s';
+    }
 });
 
 
