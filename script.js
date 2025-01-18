a8feb34da23fce9fd81a1fdf493bd59b6b0dd8d8
Kadir-23 <kadir.tastepe@stud.uni-heidelberg.de>
2025-01-18 21:23:18 +0100

    Color Model4

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ toggle.addEventListener('click', function(){
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'url("pics/background.png")'; // Set the PNG background
         body.style.backgroundSize = 'Cover'; // Make sure the background covers the body
-        body.style.color = 'black';
+        body.style.background = 'url("pics/negative.png")'; // Set the PNG background
+        body.style.backgroundSize = 'Cover'; // Make sure the background covers the body
         body.style.transition = '1s';
     }else{
         body.style.background = 'url("pics/background.png")'; // Set the PNG background
