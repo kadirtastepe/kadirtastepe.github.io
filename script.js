1fb6d05a7adcde659d02c66e78fa274ddc33aab5
Kadir-23 <kadir.tastepe@stud.uni-heidelberg.de>
2025-10-16 16:54:13 +0200

    Updated

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const roadSoFarData = [
     year: '2023',
     image: 'pics/Bosseln.png',
     alt: 'Heidelberg, Germany 2023',
-    caption: 'Heidelberg, Germany 2023'
+    caption: 'Heidelberg, Germany 2023 - Boßeln Day'
   },
   {
     year: '2023',
